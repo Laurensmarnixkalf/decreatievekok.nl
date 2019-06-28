2c1d988cc508ca513c3d322b5d9e7e01bab2c296
Laurensmarnixkalf <laurenskalf@gmail.com>
2019-06-28 10:56:06 +0200

    Changed a lot, added a new contact form

--- js/kok.js
+++ js/kok.js
@@ -1,12 +1,12 @@
-var headerContainer = document.getElementsByClassName('site-header');
-console.log(headerContainer);
+// var headerContainer = document.getElementsByClassName('site-header');
+// console.log(headerContainer);
 
-window.addEventListener("scroll",function(){
-    var target = document.getElementsByClassName('site-header');
-    if(window.pageYOffset > 50){
-     target[0].style.display = "block"; 
-    }
-    else if(window.pageYOffset < 50){
-      target[0].style.display = "none";
-    }
-  },false);
+// window.addEventListener("scroll",function(){
+//     var target = document.getElementsByClassName('site-header');
+//     if(window.pageYOffset > 50){
+//      target[0].style.display = "block"; 
+//     }
+//     else if(window.pageYOffset < 50){
+//       target[0].style.display = "none";
+//     }
+//   },false);
